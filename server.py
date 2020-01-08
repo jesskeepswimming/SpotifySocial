bc4d79e5dd413676251fde83aedfca466b10f7d6
jesskeepswimming <44094521+jesskeepswimming@users.noreply.github.com>
2020-01-08 01:08:31 -0500

    interface

--- server.py
+++ server.py
@@ -1,11 +1,15 @@
 from flask import *
-from api_interface import get_currently_playing
+from api_interface import refresh
 
 app = Flask(__name__) # instantiates a flask object in app keyword
 
-@app.route('/')
+@app.route('/', methods = ['POST'])
 def index():
-	return render_template('/index.html', data = get_currently_playing())
+	return render_template('/index.html')
+
+@app.route('/refresh', methods = ['POST'])
+def refresh():
+	return render_template('/index.html', data = refresh())
 
 if __name__ == '__main__':
     app.run(debug=True)
