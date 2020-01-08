a0df40689e6d69b67c4e4884071a55f45fb24442
jesskeepswimming <44094521+jesskeepswimming@users.noreply.github.com>
2020-01-08 00:17:55 -0500

    sending works

--- api_interface.py
+++ api_interface.py
@@ -11,35 +11,46 @@ headers = {'Content-Type': 'application/json',
             'Accept' : "application/json",
            'Authorization': 'Bearer {0}'.format(api_token)}
 
+headers2 = {'Content-Type': 'application/json',
+            'Accept' : "application/json",
+           'Authorization': 'Bearer {0}'.format(api_token2)}
+
 def get_currently_playing():
 
     response = requests.get(api_url_base+"/player/currently-playing", headers=headers)
-
-    #print(response.timestamp)
-    #print(response.id)
-
-    time=""
-    songid = ""
 
     if response.status_code == 200:
         return response
     else:
         return None
 
-print(get_currently_playing()["timestamp"])
 
-
-
-def play():  
+def play(songid, songposition):  
     
     data = {
-        "uris":["spotify:track:75ls0gurX68lUmMjE7QcsE"],
-        "position_ms": 15000
+        "uris":["spotify:track:" + songid],
+        "position_ms": songposition
     }
 
-    response = requests.put(api_url_base + "/player/play", data = json.dumps(data), headers=headers)
+    response = requests.put(api_url_base + "/player/play", data = json.dumps(data), headers=headers2)
 
     return response
+
+
+#if refresh
+
+def refresh():
+    songposition = get_currently_playing().json()["progress_ms"]
+    songid = get_currently_playing().json()["item"]["id"]
+
+    play(songid, songposition)
+
+    return
+
+refresh()
+
+
+
 
 
 
